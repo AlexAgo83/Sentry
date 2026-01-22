80e515ad28796392a1a2c4f4db5fc6ddf4df6536
Alexandre Agostini <a.agostini.fr@gmail.com>
2026-01-22 03:03:39 +0100

    data: add higher-yield herbalism herbs recipes

--- src/data/definitions/recipes.ts
+++ src/data/definitions/recipes.ts
@@ -296,6 +296,27 @@ const RECIPES_BY_SKILL: Record<SkillId, RecipeDefinition[]> = {
             itemRewards: withRewards({ herbs: 1 })
         },
         {
+            id: "herbalism_foraged_bundles",
+            skillId: "Herbalism",
+            name: "Foraged Bundles",
+            unlockLevel: unlockTier(10),
+            itemRewards: withRewards({ herbs: 2 })
+        },
+        {
+            id: "herbalism_wild_harvest",
+            skillId: "Herbalism",
+            name: "Wild Harvest",
+            unlockLevel: unlockTier(20),
+            itemRewards: withRewards({ herbs: 3 })
+        },
+        {
+            id: "herbalism_heroic_bloom",
+            skillId: "Herbalism",
+            name: "Heroic Bloom",
+            unlockLevel: unlockTier(30),
+            itemRewards: withRewards({ herbs: 4 })
+        },
+        {
             id: "herbalism_brew_tea",
             skillId: "Herbalism",
             name: "Brew Tea",
