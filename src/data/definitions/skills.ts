7bba84cc68ec7ebc156bf6a13c75f1dd8963d489
Alexandre Agostini <a.agostini.fr@gmail.com>
2026-02-08 14:17:34 +0100

    Reorder skill list

--- src/data/definitions/skills.ts
+++ src/data/definitions/skills.ts
@@ -4,18 +4,18 @@ export const SKILL_DEFINITIONS: SkillDefinition[] = [
     { id: "CombatMelee", name: "Combat - Melee", baseInterval: 5000 },
     { id: "CombatRanged", name: "Combat - Ranged", baseInterval: 5000 },
     { id: "CombatMagic", name: "Combat - Magic", baseInterval: 5000 },
-    { id: "Roaming", name: "Roaming", baseInterval: 5000 },
     { id: "Hunting", name: "Hunting", baseInterval: 2500 },
+    { id: "Fishing", name: "Fishing", baseInterval: 2500 },
     { id: "Cooking", name: "Cooking", baseInterval: 3000 },
+    { id: "Herbalism", name: "Herbalism", baseInterval: 2500 },
+    { id: "Alchemy", name: "Alchemy", baseInterval: 3000 },
+    { id: "Tailoring", name: "Tailoring", baseInterval: 3500 },
+    { id: "Leatherworking", name: "Tanning", baseInterval: 3500 },
     { id: "Excavation", name: "Excavation", baseInterval: 2000 },
     { id: "MetalWork", name: "Metalwork", baseInterval: 3500 },
-    { id: "Alchemy", name: "Alchemy", baseInterval: 3000 },
-    { id: "Herbalism", name: "Herbalism", baseInterval: 2500 },
-    { id: "Tailoring", name: "Tailoring", baseInterval: 3500 },
-    { id: "Fishing", name: "Fishing", baseInterval: 2500 },
     { id: "Carpentry", name: "Engineering", baseInterval: 3500 },
-    { id: "Leatherworking", name: "Tanning", baseInterval: 3500 },
-    { id: "Invocation", name: "Invocation", baseInterval: 3500 }
+    { id: "Invocation", name: "Invocation", baseInterval: 3500 },
+    { id: "Roaming", name: "Roaming", baseInterval: 5000 }
 ];
 
 const SKILL_BY_ID = SKILL_DEFINITIONS.reduce<Record<SkillId, SkillDefinition>>((acc, skill) => {
