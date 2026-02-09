4762cc60906968ccd530f85acabba2b34abaa8c5
Alexandre Agostini <a.agostini.fr@gmail.com>
2026-02-09 11:25:04 +0100

    Fix system icon asset usage

--- src/app/ui/systemIcon.tsx
+++ src/app/ui/systemIcon.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 const UI_ICON_PATH = `${__ASSETS_PATH__}icons/ui/`;
 
 export const SystemIcon = memo(({ className }: { className?: string }) => (
-    <svg className={className} viewBox="0 0 24 24" aria-hidden="true">
+    <svg className={className} viewBox="0 0 600 600" aria-hidden="true">
         <use href={`${UI_ICON_PATH}system.svg#icon`} />
     </svg>
 ));
