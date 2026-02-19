75fcc857e88a9c39f6d94f28e2b4078e9a53089e
Alexandre Agostini <a.agostini.fr@gmail.com>
2026-02-19 08:28:40 +0100

    fix: clean cloud save panel after autosync/reco removal

--- src/app/components/CloudSavePanel.tsx
+++ src/app/components/CloudSavePanel.tsx
@@ -80,8 +80,6 @@ export const CloudSavePanel = memo(({
     localMeta,
     cloudMeta,
     lastSyncAt,
-    localHasActiveDungeonRun,
-    cloudHasActiveDungeonRun,
     autoSyncEnabled,
     autoSyncStatus,
     autoSyncConflict,
